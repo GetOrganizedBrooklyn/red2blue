f06c0c6fdd37c6da466849cd1facab2df89bc325
Dylan Simon <dylan@dylex.net>
2019-06-02 17:14:38 -0400

    Simple populated form submitting to existing form

--- assignment-form.py
+++ assignment-form.py
@@ -13,7 +13,7 @@ TEXTER_LIST = 'TexterList'
 CAMPAIGN_LIST = 'CampaignList'
 AVAILABLE_TEXTS = 'AvailableTexts'
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, template_folder='.')
 
 redis_url = os.environ.get('REDIS_URL')
 if redis_url:
@@ -46,7 +46,13 @@ def set_state(var:str, data: bytes):
         f.write(data)
 
 
+secret = get_state('secret')
+if not secret:
+    secret = os.urandom(32)
+    set_state('secret', secret)
+app.secret_key = secret
 SHEET_ID = get_state('sheet_id').decode().strip()
+FORM_ID = get_state('form_id').decode().strip()
 
 class NotActive(Exception):
     def __init__(self):
@@ -73,6 +79,24 @@ def get_spreadsheets(creds=None):
     spreadsheets = googleapiclient.discovery.build('sheets', 'v4', credentials=creds).spreadsheets()
     return spreadsheets
 
+def get_range(range):
+    spreadsheets = get_spreadsheets()
+    res = spreadsheets.values().get(spreadsheetId = SHEET_ID, range = range).execute()
+    return res['values']
+
+def get_column(range):
+    data = get_range(range)
+    data.pop(0)
+    return [row[0] for row in data]
+
+def get_texters():
+    return get_column(TEXTER_LIST)
+
+def get_campaigns():
+    names = get_column(CAMPAIGN_LIST)
+    counts = map(int, get_column(AVAILABLE_TEXTS))
+    return zip(names, counts)
+
 def oauth_flow(**kwargs):
     client_config = json.loads(get_state('client_secret'))
     return google_auth_oauthlib.flow.Flow.from_client_config(client_config,
@@ -83,12 +107,16 @@ def oauth_flow(**kwargs):
 @app.route('/activate')
 def activate():
     flow = oauth_flow()
-    authorization_url, state = flow.authorization_url(access_type='offline')
+    authorization_url, state = flow.authorization_url(
+            access_type='offline')
+            #include_granted_scopes='true')
+    flask.session['state'] = state
     return flask.redirect(authorization_url)
 
 @app.route('/oauth2callback')
 def oauth2callback():
-    flow = oauth_flow()
+    state = flask.session['state']
+    flow = oauth_flow(state=state)
     flow.fetch_token(authorization_response = flask.request.url)
     creds = flow.credentials
 
@@ -104,7 +132,15 @@ def oauth2callback():
 
 @app.route('/')
 def top():
-    spreadsheets = get_spreadsheets()
-    res = spreadsheets.get(spreadsheetId = SHEET_ID).execute()
-    return flask.jsonify(res)
+    texters = get_texters()
+    campaigns = get_campaigns()
+    return flask.render_template('assignment-form.html', FORM_ID = FORM_ID,
+            texters = texters,
+            campaigns = campaigns)
+
+@app.route('/fake')
+def fake():
+    return flask.render_template('assignment-form.html', FORM_ID = FORM_ID,
+            texters = ['Josh Molho'],
+            campaigns = [('MONDAY 10am ET: Lauren Underwood IL', 10000)])
 
